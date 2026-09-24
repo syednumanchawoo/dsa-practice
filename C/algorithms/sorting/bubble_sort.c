f2d2f52f671a1f94a1a651d33a59bde31118e161
Syed Numan <syednumanchawoo@gmail.com>
2026-09-24 07:32:30 +0530

    Bubble Sort

--- C/algorithms/sorting/bubble_sort.c
+++ C/algorithms/sorting/bubble_sort.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-#include "../../include/array_utils.h"
+#include<stdbool.h>
 
-int main(){
-    int numbers[5];
-    inputArray(numbers, 5);
-    return 0;
+void swap(int* a, int* b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubble_sort(int arr[], int n){
+    bool swapped = true;
+    for(int i = 0; i<n; i++){
+        if(swapped == false){
+            break;
+        }
+        swapped = false;
+        for(int j = 0; j < n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                swap(&arr[j], &arr[j+1]);
+                swapped = true;
+            }
+        }
+    }
 }
